07b583be38600084111a82b421b20bf6896256c4
Rebikrrt399 <rebirthpara13@gmail.com>
2025-07-11 00:56:06 +0530

    Bikram 11_07_2025
    try to connect all the pages up and display it

---
+++ wellnessai/frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {routes} from "./routes/routes";
+import ChatPage from "./app/chat";
+
+function App() {
+  return (
+  <ChatPage />
+)}
+
+export default App;
---
+++ wellnessai/frontend/src/routes/routes.tsx
@@ -0,0 +1,7 @@
+import Chatpage from "../app/chat"
+import Testu from "../app/test"
+
+export const routes = [
+  {path:"/", element: <Chatpage />},
+  {path:"/test", element: <Testu />}
+];
